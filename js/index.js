8b274370f0ff20520a5050dbe40befc7a2657fd0
Toby Flanagan <sc21tf@leeds.ac.uk>
2023-06-25 20:35:09 +0100

    code

--- js/index.js
+++ js/index.js
@@ -11,13 +11,36 @@ function showImage(imgId, imgSrc){
     showImage.style.width = 30 + "vw";
     showImage.style.top = 20 + "vh";
     showImage.style.left = 65 + "vw";
+    showImage.style.opacity = 0;
     image.append(showImage);
+    fadeIn(showImage);
     
+}
+
+function fadeIn(image){
+
+    let opacity = 0.00
+
+    const interval = setInterval(()=>{
+
+        if(opacity >= 1){
+
+            clearInterval(interval);
+            return;
+
+        }
+        
+        opacity = 0.01 + opacity
+
+        image.style.opacity = opacity;
+
+    }, 10)
+
 }
 
 function hideImage(imgId){
 
-    console.log("moise is off text");
+    console.log("Mouse is off text");
     const image = document.getElementById(imgId);
     while(image.childElementCount > 0){
         image.removeChild(image.lastChild);
