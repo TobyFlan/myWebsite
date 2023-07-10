161cd63c2a1c3920524d99f260c4312ed8bae6cc
Toby Flanagan <sc21tf@leeds.ac.uk>
2023-07-10 20:34:04 +0100

    changes (im losing my mind)

--- js/projects.js
+++ js/projects.js
@@ -6,56 +6,7 @@ function setLayout(){
 
     console.log("current width = " + width);
 
-    const images = documents.querySelectorAll('project-image-wide');
-    const headers = documents.querySelectorAll('project-header-wide');
-    const maintexts = documents.querySelectorAll('project-text-wide');
-
-
-    images.forEach((img) => {
-
-        if(width < 740){
-
-            img.classList.add("project-image-slim");
-
-        }
-        else{
-
-            img.classList.remove("project-image-slim");
-
-        }
-
-    });
-
-
-    headers.forEach((img) => {
-
-        if(width < 740){
-
-            img.classList.add("project-header-slim");
-
-        }
-        else{
-
-            img.classList.remove("project-header-slim");
-
-        }
-
-    });
-
-    maintexts.forEach((img) => {
-
-        if(width < 740){
-
-            img.classList.add("project-text-slim");
-
-        }
-        else{
-
-            img.classList.remove("project-text-slim");
-
-        }
-
-    });
+    
 
 }
 
