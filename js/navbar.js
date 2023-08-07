24a32bc45cf4fc0381d5551aff0be21920454a2d
Toby Flanagan <sc21tf@leeds.ac.uk>
2023-08-07 14:20:05 +0100

    navbar

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+function toggleNavbar(){
+
+    var navbar = document.querySelector('.navbar');
+    var icon = document.querySelector('.navbar-button');
+
+    navbar.classList.toggle('show');
+    icon.classList.toggle('active');
+
+}
